5789f95b8e9cbee200c068637af1ca4a9d54f7f3
GiorgiaQuibanLagrisola <lagrisola.giorgia@gmail.com>
2023-12-20 17:11:27 +0100

    Random numers

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 /*
 
-    L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
-    Ogni cella ha un numero progressivo, da 1 a 100.
-    Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-    Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+    Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell'array delle bombe non potranno esserci due numeri uguali.
+    In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+    La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+    Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l'utente ha cliccato su una cella che non era una bomba.
 
 */
 
@@ -12,6 +12,18 @@ console.log('startButton', startButton, typeof startButton);
 
 startButton.addEventListener('click', function() {
     //console.log('bottone click');
+
+    const bombs = [];
+    while (bombs.length < 16) {
+        const randomNumber = getRandomNumber(1, 100);
+        console.log('randomNumber', randomNumber, typeof randomNumber);
+
+        if (bombs.includes(randomNumber) == false) {
+            bombs.push(randomNumber);
+        };
+        
+    }
+    console.log('bombs', bombs, typeof bombs);
 
     const grid = document.querySelector('.grid');
     console.log('grid', grid, typeof grid);
@@ -26,6 +38,10 @@ startButton.addEventListener('click', function() {
             console.log(cell.innerHTML);
         })
         grid.append(cell);
-    }
+    } 
 
 });
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
